dafec86dec888e78d62e4a5f698bda925fddd872
zmsvovo <zmsvovo@gmail.com>
2018-09-22 15:17:40 +0800

    Initial commit

---
+++ src/main/java/com/vovo/springbootframe/SpringbootFrameApplication.java
@@ -0,0 +1,12 @@
+package com.vovo.springbootframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFrameApplication.class, args);
+    }
+}
